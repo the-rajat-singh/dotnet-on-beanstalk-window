49b56cf1421100fe9c1f4b26ae82ae9874d7a0d5
Rajat Singh <rs9460365919@gmail.com>
2026-01-07 11:38:07 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,12 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using NotesApp.Models;
 
-namespace NotesApp.Controllers;
-
-public class HomeController : Controller
+namespace NotesApp.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Index()
+        {
+            return Content("Notes App is running successfully 🚀");
+        }
     }
 }
